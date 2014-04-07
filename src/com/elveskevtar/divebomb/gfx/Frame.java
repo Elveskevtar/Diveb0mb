1028b09e6c9360333f9fa3b1a5b0b04291105935
Elveskevtar <kevtar@gmail.com>
2014-04-07 19:49:27 -0400

    Create Frame.java

---
+++ src/com/elveskevtar/divebomb/gfx/Frame.java
@@ -0,0 +1,30 @@
+package com.elveskevtar.divebomb.gfx;
+
+import java.awt.Toolkit;
+
+import javax.swing.JFrame;
+
+public class Frame extends JFrame {
+
+	private static final long serialVersionUID = -5352440889178544005L;
+
+	public Frame() {
+		super();
+		this.setUndecorated(true);
+		this.setSize(Toolkit.getDefaultToolkit().getScreenSize());
+		this.setLocationRelativeTo(null);
+		this.setLayout(null);
+		this.add(new GameDeathmatchMP());
+		this.setVisible(true);
+	}
+
+	public Frame(int width, int height) {
+		super();
+		this.setSize(width, height);
+		this.setLocationRelativeTo(null);
+		this.setLayout(null);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.add(new GameDeathmatchMP());
+		this.setVisible(true);
+	}
+}

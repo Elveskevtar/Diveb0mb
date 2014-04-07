8583050073c2c199581e603ecaad141a6bc03bb5
Elveskevtar <kevtar@gmail.com>
2014-04-07 19:56:25 -0400

    Create Weapon.java

---
+++ src/com/elveskevtar/divebomb/weapons/Weapon.java
@@ -0,0 +1,157 @@
+package com.elveskevtar.divebomb.weapons;
+
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+
+import com.elveskevtar.divebomb.race.Player;
+
+public abstract class Weapon {
+
+	private BufferedImage image;
+	private BufferedImage sprite;
+	private Player player;
+
+	private int damage;
+	private int defense;
+	private int staminaUsage;
+	private int holdingRightX;
+	private int holdingRightY;
+	private int holdingLeftX;
+	private int holdingLeftY;
+	private int xAdjustment;
+	private int yAdjustment;
+	private int width;
+	private int height;
+	private int spriteX;
+	private int spriteY;
+
+	public abstract void attack(ArrayList<Player> players);
+
+	public Weapon(Player p) {
+		this.player = p;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+
+	public int getDefense() {
+		return defense;
+	}
+
+	public void setDefense(int defense) {
+		this.defense = defense;
+	}
+
+	public int getStaminaUsage() {
+		return staminaUsage;
+	}
+
+	public void setStaminaUsage(int staminaUsage) {
+		this.staminaUsage = staminaUsage;
+	}
+
+	public BufferedImage getImage() {
+		return image;
+	}
+
+	public void setImage(BufferedImage image) {
+		this.image = image;
+	}
+
+	public int getyAdjustment() {
+		return yAdjustment;
+	}
+
+	public void setyAdjustment(int yAdjustment) {
+		this.yAdjustment = yAdjustment;
+	}
+
+	public int getxAdjustment() {
+		return xAdjustment;
+	}
+
+	public void setxAdjustment(int xAdjustment) {
+		this.xAdjustment = xAdjustment;
+	}
+
+	public BufferedImage getSprite() {
+		return sprite;
+	}
+
+	public void setSprite(BufferedImage sprite) {
+		this.sprite = sprite;
+	}
+
+	public int getWidth() {
+		return width;
+	}
+
+	public void setWidth(int width) {
+		this.width = width;
+	}
+
+	public int getHeight() {
+		return height;
+	}
+
+	public void setHeight(int height) {
+		this.height = height;
+	}
+
+	public int getSpriteX() {
+		return spriteX;
+	}
+
+	public void setSpriteX(int spriteX) {
+		this.spriteX = spriteX;
+	}
+
+	public int getSpriteY() {
+		return spriteY;
+	}
+
+	public void setSpriteY(int spriteY) {
+		this.spriteY = spriteY;
+	}
+
+	public int getHoldingRightX() {
+		return holdingRightX;
+	}
+
+	public void setHoldingRightX(int holdingRightX) {
+		this.holdingRightX = holdingRightX;
+	}
+
+	public int getHoldingRightY() {
+		return holdingRightY;
+	}
+
+	public void setHoldingRightY(int holdingRightY) {
+		this.holdingRightY = holdingRightY;
+	}
+
+	public int getHoldingLeftX() {
+		return holdingLeftX;
+	}
+
+	public void setHoldingLeftX(int holdingLeftX) {
+		this.holdingLeftX = holdingLeftX;
+	}
+
+	public int getHoldingLeftY() {
+		return holdingLeftY;
+	}
+
+	public void setHoldingLeftY(int holdingLeftY) {
+		this.holdingLeftY = holdingLeftY;
+	}
+}

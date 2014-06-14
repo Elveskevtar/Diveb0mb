c7dc94447174b772846f06b3d45b050159785265
Elveskevtar <Elveskevtar@users.noreply.github.com>
2014-06-14 18:46:45 -0400

    Update Melee.java

--- src/com/elveskevtar/divebomb/weapons/Melee.java
+++ src/com/elveskevtar/divebomb/weapons/Melee.java
@@ -1,9 +1,13 @@
 package com.elveskevtar.divebomb.weapons;
 
+import java.util.ArrayList;
+
+import com.elveskevtar.divebomb.net.packets.Packet04Attack;
+import com.elveskevtar.divebomb.net.packets.Packet06Kill;
 import com.elveskevtar.divebomb.race.Player;
 
 public abstract class Melee extends Weapon {
-	
+
 	private int distance;
 
 	public Melee(Player p) {
@@ -16,5 +20,52 @@ public abstract class Melee extends Weapon {
 
 	public void setDistance(int distance) {
 		this.distance = distance;
+	}
+
+	@Override
+	public void attack(ArrayList<Player> players, boolean server) {
+		if (getPlayer().getGame().getSocketClient() == null || server == true) {
+			ArrayList<Player> attackedPlayers = new ArrayList<Player>();
+			for (Player p : players)
+				if (p != getPlayer() && !p.isDead()) {
+					if (getPlayer().isFacingRight()
+							&& p.getxPosition() - getPlayer().getxPosition() <= 0
+							&& Math.sqrt(Math.pow(
+									(p.getxPosition() - getPlayer()
+											.getxPosition()), 2)
+									+ Math.pow((p.getyPosition() - getPlayer()
+											.getyPosition()), 2)) <= getDistance())
+						attackedPlayers.add(p);
+					if (!getPlayer().isFacingRight()
+							&& p.getxPosition() - getPlayer().getxPosition() >= 0
+							&& Math.sqrt(Math.pow(
+									(p.getxPosition() - getPlayer()
+											.getxPosition()), 2)
+									+ Math.pow((p.getyPosition() - getPlayer()
+											.getyPosition()), 2)) <= getDistance())
+						attackedPlayers.add(p);
+				}
+			for (Player p : attackedPlayers) {
+				p.setHealth(p.getHealth() + (Math.random() * -10)
+						+ p.getInHand().getDefense() - getDamage());
+				if (p.getHealth() <= 0) {
+					getPlayer().setKills(getPlayer().getKills() + 1);
+					p.setDeaths(p.getDeaths() + 1);
+					if (server) {
+						Packet06Kill packet = new Packet06Kill(getPlayer()
+								.getName(), p.getName());
+						packet.writeData(getPlayer().getGame()
+								.getSocketServer());
+					}
+				}
+			}
+			for (int i = 0; i < attackedPlayers.size(); i++)
+				attackedPlayers.remove(i);
+		} else if (getPlayer().getName().equalsIgnoreCase(
+				getPlayer().getGame().getUser().getName())
+				&& !server) {
+			Packet04Attack packet = new Packet04Attack(getPlayer().getName());
+			packet.writeData(getPlayer().getGame().getSocketClient());
+		}
 	}
 }

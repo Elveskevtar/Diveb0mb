312f262db6ec975dbff2094ce734171d8c6933a0
Elveskevtar <Elveskevtar@users.noreply.github.com>
2014-06-14 18:37:56 -0400

    Update SpawnPoints.java

--- src/com/elveskevtar/divebomb/maps/SpawnPoints.java
+++ src/com/elveskevtar/divebomb/maps/SpawnPoints.java
@@ -2,19 +2,21 @@ package com.elveskevtar.divebomb.maps;
 
 public enum SpawnPoints {
 
-	TESTMAP1(0, 1, -160, -1056),
-	TESTMAP2(0, 1, 0, -1056),
-	TESTMAP3(0, 2, -1000, -1056),
-	TESTMAP4(0, 2, -1128, -1056);
+	TESTMAP1(0, 1, 1, -160, -1056),
+	TESTMAP2(0, 1, 2, 0, -1056),
+	TESTMAP3(0, 2, 3, -1000, -1056),
+	TESTMAP4(0, 2, 4, -1128, -1056);
 	
 	private int mapID;
 	private int team;
+	private int spawnID;
 	private double x;
 	private double y;
 	
-	SpawnPoints(int mapID, int team, double x, double y) {
+	SpawnPoints(int mapID, int team, int spawnID, double x, double y) {
 		this.mapID = mapID;
 		this.team = team;
+		this.spawnID = spawnID;
 		this.x = x;
 		this.y = y;
 	}
@@ -49,5 +51,13 @@ public enum SpawnPoints {
 
 	public void setY(double y) {
 		this.y = y;
+	}
+
+	public int getSpawnID() {
+		return spawnID;
+	}
+
+	public void setSpawnID(int spawnID) {
+		this.spawnID = spawnID;
 	}
 }
